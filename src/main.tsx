42f25218cf86a9089635ea34735448627675a44f
t86162100-byte <t86162100@gmail.com>
2026-09-15 23:44:49 +0100

    Mount glass action bar above navigation

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './mobile.css';
 import './tabNavigation';
 import './chartToolbarCleanup';
 import './removeMobileChartControls';
+import './glassActionBar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
