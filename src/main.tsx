404cdf33e92cd714f738dafbdc472b851293cbb3
t86162100-byte <t86162100@gmail.com>
2026-09-15 15:48:36 +0100

    Wire bottom tab navigation into app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './tabNavigation';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
