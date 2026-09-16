e9c5e356023a5a8cb441c640f13e925be026d3e6
t86162100-byte <t86162100@gmail.com>
2026-09-16 16:39:57 +0100

    Remove legacy candle renderers and load native candlestick renderer

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './candleFix.css';
 import './mobile.css';
 import './tabNavigation';
 import './chartToolbarCleanup';
@@ -28,7 +27,7 @@ import './chartDefaults';
 import './sireLoadingOverlay';
 import './sireChatLoader';
 import './pricePaneVerticalZoom';
-import './candleRendererFix';
+import './nativeCandlestickBridge';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
