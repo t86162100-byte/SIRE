6c2c19272083abfc92daa878b211a9eaa4db4a39
t86162100-byte <t86162100@gmail.com>
2026-09-16 07:48:52 +0100

    Use universal draw icon instead of Draw text

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './glassTimeframeSafe';
 import './glassIndicatorBridge';
 import './glassIndicatorIconFix';
 import './glassTimeframeStyleFix';
+import './glassDrawIconFix';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
