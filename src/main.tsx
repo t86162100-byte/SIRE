0041359cd50e8dad36f18c6f7daebc79a1fd3d39
t86162100-byte <t86162100@gmail.com>
2026-09-16 18:50:45 +0100

    Remove recreated glass chart bar

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,6 @@ import './quoteListFullHeight';
 import './sireLoadingOverlay';
 import './sireChatLoader';
 import './glassActionBar';
-import './chartGlassBarRestore';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
