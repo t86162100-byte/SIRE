43324107eb3c3131734b5b9493ae9277fa724212
t86162100-byte <t86162100@gmail.com>
2026-09-16 15:48:03 +0100

    Apply candle rendering fix after base chart styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './candleFix.css';
 import './mobile.css';
 import './tabNavigation';
 import './chartToolbarCleanup';
