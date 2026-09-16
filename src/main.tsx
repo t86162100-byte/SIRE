ba7d0ae6fcd62021d00847cee74063a204157ddd
t86162100-byte <t86162100@gmail.com>
2026-09-16 17:42:58 +0100

    Remove legacy chart tool bridges from app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,6 @@ import App from './App';
 import './index.css';
 import './mobile.css';
 import './tabNavigation';
-import './chartToolbarCleanup';
-import './removeMobileChartControls';
-import './glassActionBar';
-import './glassInstrumentBridge';
-import './glassInstrumentSwipeFix';
-import './glassInstrumentHold';
-import './glassTimeframeSafe';
-import './glassIndicatorBridge';
-import './glassIndicatorIconFix';
-import './glassTimeframeStyleFix';
-import './glassDrawIconFix';
-import './glassDrawBridge';
-import './glassToolsBridge';
 import './navigationAutoHide';
 import './tabLayoutController';
 import './quoteLogoHeader';
@@ -26,7 +13,6 @@ import './quoteListFullHeight';
 import './chartDefaults';
 import './sireLoadingOverlay';
 import './sireChatLoader';
-import './nativeCandlestickBridge';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
