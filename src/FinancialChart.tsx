126788d684a43ae619c3dce9b3e46f0e33fe7aca
t86162100-byte <t86162100@gmail.com>
2026-09-18 10:06:01 +0100

    Add more timeframe intervals

--- src/FinancialChart.tsx
+++ src/FinancialChart.tsx
@@ -32,8 +32,9 @@ const DRAW_RACK_GROUPS: DrawGroup[] = [
 ];
 
 const INTERVAL_SECONDS: Record<string, number> = {
-  '1m': 60, '2m': 120, '3m': 180, '5m': 300, '10m': 600, '15m': 900,
-  '30m': 1800, '1h': 3600, '2h': 7200, '4h': 14400, '1d': 86400, '1w': 604800,
+  '1m': 60, '2m': 120, '3m': 180, '5m': 300, '10m': 600, '15m': 900, '20m': 1200,
+  '30m': 1800, '45m': 2700, '1h': 3600, '2h': 7200, '3h': 10800, '4h': 14400,
+  '6h': 21600, '8h': 28800, '12h': 43200, '1d': 86400, '1w': 604800,
 };
 const DERIV_INTERVALS = Object.keys(INTERVAL_SECONDS);
 for (const [code, seconds] of Object.entries(INTERVAL_SECONDS)) {
