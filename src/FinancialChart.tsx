63c94cdde136521dc1ffb8a7c9b98c49e6706487
t86162100-byte <t86162100@gmail.com>
2026-09-18 17:41:55 +0100

    Make OBJ button uppercase

--- src/FinancialChart.tsx
+++ src/FinancialChart.tsx
@@ -1179,7 +1179,7 @@ export default function FinancialChart({ symbol, isActive = false, liveTick, req
             title="Objects"
             onClick={() => widgetRef.current?.openObjects()}
           >
-            <span aria-hidden="true">Obj</span>
+            <span aria-hidden="true">OBJ</span>
           </button>
           <button
             type="button"
