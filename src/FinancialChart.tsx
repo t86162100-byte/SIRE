318b0641f502659cdafa243731fd16bd50ce090f
t86162100-byte <t86162100@gmail.com>
2026-09-18 11:03:53 +0100

    Lower drawing icon wave while keeping pencil position

--- src/FinancialChart.tsx
+++ src/FinancialChart.tsx
@@ -749,7 +749,7 @@ export default function FinancialChart({ symbol, liveTick, requestHistory, instr
           onClick={() => setDrawRackOpen(open => !open)}
         >
           <svg className="sire-bottom-draw-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-            <path d="M2 18c3-3 6-3 9 0s6 3 9 0" />
+            <path d="M2 20c3-3 6-3 9 0s6 3 9 0" />
             <path d="M21 3l-1.7-1.7L5.2 15.4 3.5 20.5l5.1-1.7L21 6.4z" />
             <path d="M16.9 4.7l2.4 2.4" />
           </svg>
